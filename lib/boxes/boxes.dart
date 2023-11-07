90730d9e33f50ef8ca19f6578ebbdd97c14a9816
vikash <vikashyadav20072002@gmail.com>
2023-11-07 23:48:30 +0530

    helli Hive

---
+++ lib/boxes/boxes.dart
@@ -0,0 +1,14 @@
+
+
+
+
+import 'package:hive/hive.dart';
+
+import '../models/notes_model.dart';
+
+class Boxes {
+
+  static Box<NotesModel> getData() => Hive.box<NotesModel>('notes');
+  //static kaam ka ho sakta hai
+
+}
